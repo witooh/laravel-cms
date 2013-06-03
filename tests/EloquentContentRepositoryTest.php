77f26b111135a8c7215c06837b400f332ec333e2
witooh <witoo@outlook.com>
2013-06-04 00:44:07 +0700

    update test

--- tests/EloquentContentRepositoryTest.php
+++ tests/EloquentContentRepositoryTest.php
@@ -1,20 +1,58 @@
 <?php
 
-namespace Witooh\Cms\Tests;
 
 use Witooh\Cms\Models\Content;
 use Witooh\Cms\Repositories\IContentRepository;
 
-class EloquentContentRepositoryTest extends \PHPUnit_Framework_TestCase{
+class EloquentContentRepositoryTest extends TestCase {
 
+    protected $contentRepository;
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->contentRepository = $this->app['Witooh\Cms\Repositories\IContentRepository'];
+    }
+
+
+    /**
+     * Test insert data into db
+     */
     public function testCreate(){
         //arrange
-//        $content = new Content();
+        $data = array(
+            'title'=>'test',
+            'content'=>'description',
+            'published'=>true,
+        );
+        $content = new Content($data);
 
         //Act
-//        $savedContent = $this->contentRepository->create($content);
+        $savedContent = $this->contentRepository->create($content);
 
         //Assert
-//        $this->assertEqual($savedContent, $content);
+        $this->assertEquals($content, $savedContent);
+    }
+
+    /**
+     * Test update data into db
+     */
+    public function testUpdate(){
+        //arrange
+        $contentRepository = Mockery::mock('Witooh\Cms\Repositories\IContentRepository');
+        $contentRepository->shouldReceive('findByID')->with(Mockery::any())->once()->andReturn(new Content(array(
+                'title'=>'test',
+                'content'=>'description',
+                'published'=>true,
+            )));
+        $content = $contentRepository->findByID(1);
+        $content->title = 'Updated Title';
+
+        //Act
+        $updatedContent = $this->contentRepository->update($content);
+
+        //Assert
+        $this->assertEquals($content, $updatedContent);
     }
 }
