bf0d8ff1413d348cc8a24b923f8c68669ec7d504
witooh <witoo@outlook.com>
2013-06-03 22:11:17 +0700

    update

---
+++ tests/EloquentContentRepositoryTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Witooh\Cms\Tests;
+
+use Witooh\Cms\Models\Content;
+use Witooh\Cms\Repositories\IContentRepository;
+
+class EloquentContentRepositoryTest extends \PHPUnit_Framework_TestCase{
+
+    public function testCreate(){
+        //arrange
+//        $content = new Content();
+
+        //Act
+//        $savedContent = $this->contentRepository->create($content);
+
+        //Assert
+//        $this->assertEqual($savedContent, $content);
+    }
+}
